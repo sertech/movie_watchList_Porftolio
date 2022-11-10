76c096eed5e63f0a37c57acd5d3b565adefca405
sergio alvarez pinto <sertech_live@hotmail.com>
2022-11-09 23:33:23 -0400

    165 adding the navbar

---
+++ movie_library/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+from movie_library.routes import pages
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
+    app.config["SECRET_KEY"] = os.environ.get(
+        "SECRET_KEY", "0A_c-ekmVQ1X1pcL_3_yXprXWiOSzlCXk8r0a7av9Ns"
+    )
+    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+
+    app.register_blueprint(pages)
+    return app
