e0c4a27094e24e9545102ad8f422968d1038058d
sergio alvarez pinto <sertech_live@hotmail.com>
2022-11-11 21:41:37 -0400

    168 complete

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, request
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -8,3 +8,18 @@ pages = Blueprint(
 @pages.route("/")
 def index():
     return render_template("index.html", title="Movies Watchlist")
+
+
+@pages.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    #! passed variables from the templates that are not parameters are query values
+    print(request.args.get("query_test"))
+    
+    # all endpoints have to return something
+    return redirect(request.args.get("current_page"))
